49fbff25c437e9db30090a332d8ee09dec1eb4af
Meggie Ariana Sanchez Zambrano <meggie@nutella>
2016-11-18 18:04:38 -0400

    agregada hora al shell

--- ProyectoII/ShellScriptAlert.sh
+++ ProyectoII/ShellScriptAlert.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 a=0
+now=$(date +"%r")
 
 while [ $a ]
 do
    ping -c 3 "192.168.1.116" > /dev/null
    if [ $? != 0 ]
    then
-   	  echo "Se ha caído la conexión"
+   	  echo "Se ha caído la conexión. Hora actual: $now"
    fi
 done
